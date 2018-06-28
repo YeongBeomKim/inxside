65444aa3db3587ed4a51bd6fd59b5c1c88732ca4
김영범 <kyb5493@gmail.com>
2018-06-29 02:47:47 +0900

    Header setting, material-colors install

--- inxside-frontend/src/components/base/Header/Header.js
+++ inxside-frontend/src/components/base/Header/Header.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { type Node } from 'react';
 import Responsive from 'components/common/Responsive';
 import { Link } from 'react-router-dom';
@@ -10,10 +11,27 @@ type Props = {
 
 const Header = () => (
     <header>
-      <Responsive>
-        <div className="Header">
+        <div className="header">
+          <Responsive>
+            <Link className="logo" to="/">
+              최인애, 감정을 입다.
+            </Link>
+            <nav className="header-nav">
+              <div className="left">
+                <Link className="nav-item" to="/">ABOUT</Link>
+                <Link className="nav-item" to="/">COLLECTION</Link>
+                <Link className="nav-item" to="/">EXHIBITION</Link>
+                <Link className="nav-item" to="/">PRODUCTS</Link>
+                <Link className="nav-item" to="/">CONTACT</Link>
+              </div>
+              <div className="right">
+                <Link className="nav-item" to="/">FACEBOOK</Link>
+                <Link className="nav-item" to="/">INSTAGRAM</Link>
+              </div>
+            </nav>
+          </Responsive>
+
         </div>
-      </Responsive>
     </header>
 );
 
